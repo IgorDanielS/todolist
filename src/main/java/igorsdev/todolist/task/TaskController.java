0d3956ab192f925d44dc9f20f35b425531c591ca
IgorDanielS <igordaniel1903@gmail.com>
2025-01-28 20:09:22 -0300

    feat: post task

--- src/main/java/igorsdev/todolist/task/TaskController.java
+++ src/main/java/igorsdev/todolist/task/TaskController.java
@@ -1,5 +1,27 @@
 package igorsdev.todolist.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
     
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createTask(@RequestBody TaskModel task){
+        try {
+            taskService.create(task);
+            return ResponseEntity.ok().body(task);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }   
+
+    }
 }
