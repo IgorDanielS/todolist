ea383265afb55f9c817754bf92097c8e1390fcd9
IgorDanielS <igordaniel1903@gmail.com>
2025-02-01 11:05:10 -0300

    feat: verify dates and dealing exceptions

--- src/main/java/igorsdev/todolist/task/TaskService.java
+++ src/main/java/igorsdev/todolist/task/TaskService.java
@@ -1,5 +1,6 @@
 package igorsdev.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,17 @@ public class TaskService {
 
     public TaskModel create(TaskModel task) throws Exception{
         Optional<TaskModel> taskToFind = taskRepository.findByTitle(task.getTitle());
+        var currentDate = LocalDateTime.now();
         if(taskToFind.isPresent()){
             throw new Exception("Task with same title already existed!");
         }
+        if(currentDate.isAfter(task.getStartAt()) || currentDate.isAfter(task.getEndAt())){
+            throw new Exception("Cant attribute past dates to taks!");
+        }
+        if(task.getStartAt().isAfter(task.getEndAt())){
+            throw new Exception("Cant attribute End At before Start At date");
+        }
+    
         return taskRepository.save(task);
     }
 }
